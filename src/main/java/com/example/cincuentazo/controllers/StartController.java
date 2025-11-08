64db85b48964501406248520bc083bfd36bbf264
LGZU5 <jisusdeivid@gmail.com>
2025-11-08 16:36:32 -0500

    Implementa el menú de inicio con botones de acción para jugar, guía y salir

--- src/main/java/com/example/cincuentazo/controllers/StartController.java
+++ src/main/java/com/example/cincuentazo/controllers/StartController.java
@@ -1,5 +1,6 @@
 package com.example.cincuentazo.controllers;
 
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
@@ -13,5 +14,14 @@ public class StartController {
     @FXML
     private void onQuit() {
         System.exit(0);
+    }
+
+        public void onPlay() {
+        System.out.println("Play button clicked");
+    }
+
+
+    public void onGuia() {
+        System.out.println("Guia button clicked");
     }
 }
